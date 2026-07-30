515875f882a8938bb5cd83efa184b83fb8b026ad
Alina Hashmi <heybbyali@gmail.com>
2026-07-30 16:20:36 +0530

    added program to show size of data type

---
+++ CPP_Basics/01_input_output/sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a= 5;
+    float b = 5.5;
+    char  c = 'A';
+    double d = 5.555555;
+    cout << "size of integer is:" << sizeof(a) << endl;
+    cout << "size of float is:"<< sizeof(b) << endl;
+    cout << "size of character is:" << sizeof(c) << endl;
+    cout << "size of double is:" << sizeof(d) << endl;
+    return 0;
+}
+   
