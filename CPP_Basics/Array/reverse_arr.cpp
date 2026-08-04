2d94173b6896ca24af68eecc55573abe6e5d1cb3
Alina Hashmi <heybbyali@gmail.com>
2026-08-04 14:46:36 +0530

    added program to reverse an array

---
+++ CPP_Basics/Array/reverse_arr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i,n ; 
+    cout << "Enter size of an array : ";
+    cin >> n;
+    int arr[n];
+    for (i = 0 ; i<n ; i++){
+        cout << "enter elements in array at index  " << i << "\n";
+        cin >> arr[i];
+    }
+    for (i = (n-1); i >= 0; i--){
+
+        cout << arr[i]<< "  ";   
+    }
+
+    return 0;
+
+}
