94464742ace436b1755ba257417bac8f8250ef02
Alina Hashmi <heybbyali@gmail.com>
2026-08-12 16:43:03 +0530

    added pattern program

---
+++ CPP_Basics/patters/p5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void printPattern(int n) {
+    for (int i = 0; i < n ; i++) {
+        for (int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    printPattern(n);
+    return 0;
+
+}
