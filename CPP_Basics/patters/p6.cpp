a05e0e502212a2ac101748a52dc9bf7cc6f0510a
Alina Hashmi <heybbyali@gmail.com>
2026-08-12 16:49:34 +0530

    added pattern program

--- CPP_Basics/patters/p6.cpp
+++ CPP_Basics/patters/p6.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 void printPattern(int n) {
-    for (int i = 0; i < n ; i++) {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - i; j++) {
             cout << j + 1 << " ";
         }
@@ -15,5 +15,4 @@ int main(){
     cin >> n;
     printPattern(n);
     return 0;
-
 }
