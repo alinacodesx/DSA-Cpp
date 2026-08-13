7e4af0d9e46ddedb4e19fb96ea12df1cba161674
Alina Hashmi <heybbyali@gmail.com>
2026-08-13 22:52:21 +0530

    added pattern program

--- CPP_Basics/patterns/p8.cpp
+++ CPP_Basics/patterns/p8.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 void printPattern(int n) {
     for (int i = 0; i < n; i++) {
-        for (int k = 0; k < n - i - 1; k++) {
+        //Spaces don't "make room" for stars.They simply push the stars to the right.
+        for (int k = 0; k < i; k++) {
             cout << " ";
         }
-        for (int j = 0 ; j < i * 2+1 ; j++) {
+        for (int j = n * 2 -1-i ; j > i ; j--) {
             cout << "*" ;
         }
         cout << endl;
