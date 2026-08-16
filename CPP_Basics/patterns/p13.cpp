7cf408e72962841af4af29cc39b962cca0b3312a
Alina Hashmi <heybbyali@gmail.com>
2026-08-16 21:08:44 +0530

    added pattern program

--- CPP_Basics/patterns/p13.cpp
+++ CPP_Basics/patterns/p13.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
 void printPattern(int n) {
-    for (int i = 0; i <= n; i++) {
-            for (int k = 0; k < n; k++){
-            cout << " ";
+    int num = 1;
+    for (int i = 1; i <= n; i++) {
+             for (int j = 1; j <= i  ; j++) {
+            cout << num << " ";
+            num++;
         }
-        for (int j = i ; j > 0  ; j--) {
-            cout << j ;
-        }
-        cout << endl;
-    
-        }       
-    }
+        cout << endl;           
+}
+     
+}
 
 int main(){
     int n;
