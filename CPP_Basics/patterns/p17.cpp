653eeac1f54d65bff89ec71186225025b5aae6a5
Alina Hashmi <heybbyali@gmail.com>
2026-08-18 19:08:17 +0530

    added pattern program

--- CPP_Basics/patterns/p17.cpp
+++ CPP_Basics/patterns/p17.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 void printPattern(int n) {
-    for (int i = 1; i <= n; i++) {
-        // char ch = 'A';
-    //      for(int j = 1; j <= i; j++) {
-    //      cout << ch;
-    //      ch++;
-    // }
-    //     cout << endl;
-
-        for (int j = 1 ; j<=i ;j++){
-            cout<< char('A'+ i-1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
         }
-        cout << endl;
+        char ch = 'A';
+        for(int j = 0; j <= i; j++) {
+            cout << ch ;
+            ch++;
+        }
+          ch = ('A'+i-1);
+         for(int j = 1; j <= i; j++) {
+         cout <<ch ;
+         ch--;
     }
-     
+        cout << endl; 
+    }
 }
 
 int main(){
